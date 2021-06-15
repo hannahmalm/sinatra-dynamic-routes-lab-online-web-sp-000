23ed722d996c274fd7eb48b8cb0d6c5ddbfaafe9
Hannah M <hannahemalm616@gmail.com>
2021-06-15 00:45:36 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -42,7 +42,7 @@ class App < Sinatra::Base
     elsif @operation == "multiply"
       @number1 * @number2
     else @operation == "divide"
-      @number2 % @number1
+      @number2 / @number1
   end 
 end 
 
