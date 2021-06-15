45bb1c0c5bfa4c83e64df991893ea8e00b3e8ec6
Hannah M <hannahemalm616@gmail.com>
2021-06-15 00:50:51 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -36,7 +36,7 @@ class App < Sinatra::Base
     number2 = params[:number2].to_i
    
     if @operation == "add"
-      number1 + number2
+      (number1 + number2).to_s
     elsif @operation == "subtract"
       number2 - number1
     elsif @operation == "multiply"
